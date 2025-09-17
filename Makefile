dbb9866f98fe3abd4a88e7601994e5ea1d4e5c1d
Alberto Damo <alberto.damo@gmail.com>
2025-09-17 15:43:59 +0200

    feat(kernel.c): removed boot.s by inserting the memory layout into kernel.c. Also removing boot.s from the compilation process

--- Makefile
+++ Makefile
@@ -11,11 +11,8 @@ grub_image: build $(BIN_IMAGE)
 	$(shell  if ! grub-file --is-x86-multiboot $(BIN_IMAGE); then echo "damos.bin is not valid x86-multiboot format"; fi)
 	grub-mkrescue -o damos.iso isodir
 
-build: build_boot build_kernel linker.ld
-	./i686-elf/bin/i686-elf-gcc -T linker.ld -o $(BIN_IMAGE) -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-
-build_boot: boot.s
-	./i686-elf/bin/i686-elf-as boot.s -o boot.o
+build: build_kernel linker.ld
+	./i686-elf/bin/i686-elf-gcc -T linker.ld -o $(BIN_IMAGE) -ffreestanding -O2 -nostdlib kernel.o -lgcc
 
 build_kernel: kernel.c
 	./i686-elf/bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
--- kernel.c
+++ kernel.c
@@ -12,6 +12,44 @@
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
 
+#define V_ALIGN (1<<0)                    /* align loaded modules on page boundaries */
+#define V_MEMINFO  (1<<1)                 /* provide memory map */
+#define V_FLAGS    (V_ALIGN | V_MEMINFO)      /* this is the Multiboot 'flag' field */
+#define V_MAGIC    0x1BADB002             /* 'magic number' lets bootloader find the header */
+#define V_CHECKSUM (-(V_MAGIC + V_FLAGS))     /* checksum of above, to prove we are multiboot */
+
+//section .multiboot
+struct __attribute__((packed, aligned(16))) multiboot_header{
+  uint32_t MAGIC;
+  uint32_t FLAGS;
+  uint32_t CHECKSUM;
+};
+
+struct multiboot_header mb_header __attribute__((section(".multiboot")))= {
+  .MAGIC = V_MAGIC,
+  .FLAGS = V_FLAGS,
+  .CHECKSUM = V_CHECKSUM,
+};
+
+
+//section .bss
+uint8_t stack[16384] __attribute__((section(".bss")));
+
+void _start(void) __attribute__((naked, section(".text")));
+void _start(void)
+{
+  asm volatile ("movl %0, %%esp"
+       :
+       :"r"(stack + sizeof(stack))
+       :);
+
+  asm volatile(
+      "call kernel_main\n"
+      "cli\n"
+      "1: hlt\n"
+      "jmp 1b");
+}
+
 /* Hardware text mode color constants. */
 enum vga_color {
 	VGA_COLOR_BLACK = 0,
